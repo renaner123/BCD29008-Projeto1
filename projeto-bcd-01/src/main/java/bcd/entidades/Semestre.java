c54791ef480ccefa0999700769560617c6f93589
Renan <renaner123@gmail.com>
2021-08-20 16:49:30 -0300

    Classe usada para pegar data de inicio e fim do semestre

--- projeto-bcd-01/src/main/java/bcd/entidades/Semestre.java
+++ projeto-bcd-01/src/main/java/bcd/entidades/Semestre.java
@@ -6,7 +6,7 @@ public class Semestre {
 
     private int idSemestre;
     public static String primeiroDiaLetivo = "2021-03-1 15:30:00";
-    public static String ultimoDiaLetivo = "2021-09-1 15:30:00";
+    public static String ultimoDiaLetivo = "2021-09-28 15:30:00";
 
 
     public Semestre(int idSemestre) {

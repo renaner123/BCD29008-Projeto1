74ab1ec26aba732c861d1afb4f4149a79f79135f
Renan <renaner123@gmail.com>
2021-08-20 16:45:50 -0300

    Classe responsável pela iteracao com usuario

--- projeto-bcd-01/src/main/java/bcd/Principal.java
+++ projeto-bcd-01/src/main/java/bcd/Principal.java
@@ -1,68 +1,102 @@
 package bcd;
 
-import bcd.db.ConnectionFactory;
-import bcd.entidades.EmprestimoDAO;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import bcd.entidades.*;
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class Principal {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         boolean sair = false;
         int opcao = 0;
+        int auxFind = 0;
         EmprestimoDAO emprestimos = new EmprestimoDAO();
-        //Scanner scanner = new Scanner(System.in);
+        KitDAO kit = new KitDAO();
+        EquipamentoDAO equipamento = new EquipamentoDAO();
+        AlunosDAO aluno = new AlunosDAO();
+        Scanner scanner = new Scanner(System.in);
+        int matricula =0;
+        int atividade = 0;
+        int idEquipamento = 0;
+        int idEmprestimo =0 ;
 
-        System.out.println(emprestimos.emprestimosEmAndamento());
+        while (!sair) {
+            System.out.println(" ### Selecione uma das Opções");
+            System.out.println("\n\t## EMPRÉSTIMOS");
+            System.out.println("\t\t1 - Efetuar empréstimo");
+            System.out.println("\t\t2 - Renovar empréstimo");
+            System.out.println("\t\t3 - Finalizar empréstimo");
+            System.out.println("\t\t4 - Relatórios");
+            System.out.println("Digite uma opção: ");
+            opcao = scanner.nextInt();
 
-//        while (!sair) {
-//            System.out.println(" ### Selecione uma das Opções");
-//            System.out.println("\n\t## EMPRÉSTIMOS");
-//            System.out.println("\t\t1 - Efetuar empréstimo");
-//            System.out.println("\t\t2 - Renovar empréstimo");
-//            System.out.println("\t\t3 - Finalizar empréstimo");
-//            System.out.println("\n\t## RELATÓRIOS");
-//            System.out.println("\t\t4 - Empréstimos em andamento");
-//            System.out.println("\t\t5 - Empréstimos por equipamento");
-//            System.out.println("\t\t6 - Equipamentos emprestados por determinado aluno");
-//            System.out.println("\t\t7 - Empréstimos em andamento já vencidos");
-//            System.out.println("\n\t\t0 - Sair");
-//            System.out.println("Opção: ");
-//            opcao = scanner.nextInt();
-//
-//            switch (opcao) {
-//                case 1:
-//
-//                    break;
-//                case 2:
-//
-//                    break;
-//                case 3:
-//
-//                    break;
-//                case 4:
-//                    emprestimos.emprestimosEmAndamento();
-//                    break;
-//                case 5:
-//                case 6:
-//                case 7:
-//                    System.out.println("Digitou " + opcao);
-//                    break;
-//                case 0:
-//                    sair = true;
-//                    break;
-//                default:
-//                    System.out.println("Opção Inválida.");
-//                    break;
-//
-//            }
-//        }
+            switch (opcao) {
+                case 1:
+                    System.out.println("\t\t1 - Digite a matricula do aluno");
+                    matricula = scanner.nextInt();
+                    System.out.println("\t\t1 - Digite id da atividade");
+                    atividade = scanner.nextInt();
+                    System.out.println("\t\t1 - Digite o id do Kit ou Equipamento a ser emprestado");
+                    idEquipamento = scanner.nextInt();
+                    emprestimos.efetuarEmprestimo(matricula,idEquipamento,atividade);
+                    break;
+                case 2:
+                    System.out.println("\t\t1 - Digite o id do Emprestimo");
+                    idEmprestimo = scanner.nextInt();
+                    System.out.println("\t\t1 - Digite a matricula do aluno");
+                    matricula = scanner.nextInt();
+                    emprestimos.renovarEmprestimo(idEmprestimo,matricula);
+                    break;
+                case 3:
+                    System.out.println("\t\t1 -  Digite o id do Emprestimo");
+                    idEmprestimo = scanner.nextInt();
+                    emprestimos.finalizarEmprestimo(idEmprestimo);
+                    break;
+                case 4:
+
+                    System.out.println("\t\t1 - Empréstimos em andamento");
+                    System.out.println("\t\t2 - Empréstimos por equipamento");
+                    System.out.println("\t\t3 - Equipamentos emprestados por determinado aluno");
+                    System.out.println("\t\t4 - Empréstimos em andamento já vencidos");
+                    System.out.println("\t\t5 - Equipamentos cadatrados");
+                    System.out.println("\t\t6 - Kits cadatrados");
+                    System.out.println("\t\t7 - Alunos cadastrados");
+                    System.out.println("Digite uma opção: ");
+                    opcao = scanner.nextInt();
+
+                    switch (opcao) {
+                        case 1:
+                            System.out.println(emprestimos.emprestimosEmAndamento());
+                            break;
+                        case 2:
+                            System.out.println("Digite a ID do material");
+                            auxFind = scanner.nextInt();
+
+                            System.out.println(emprestimos.emprestimoDeterminadoMaterial(auxFind));
+                            break;
+                        case 3:
+                            System.out.println("Digite matricula do aluno");
+                            auxFind = scanner.nextInt();
+                            System.out.println(emprestimos.emprestimoDeterminadoAluno(auxFind));
+                            break;
+                        case 4:
+                            System.out.println(emprestimos.emprestimosEmAndamentoVencidos());
+                            break;
+                        case 5:
+                            System.out.println(equipamento.equipamentosEmDb());
+                            break;
+                        case 6:
+                            System.out.println(kit.kitEmDb());
+                            break;
+                        case 7:
+                            System.out.println(aluno.alunosEmDb());
+                            break;
+                    }
+            }
+        }
 
     }
 
 }
+
 

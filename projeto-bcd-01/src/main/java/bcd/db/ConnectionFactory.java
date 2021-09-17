2f17969806e0d6f3722021e59b0d8ba2179c0ae9
Renan <renaner123@gmail.com>
2021-09-17 16:01:16 -0300

    Removido URI exposed

--- projeto-bcd-01/src/main/java/bcd/db/ConnectionFactory.java
+++ projeto-bcd-01/src/main/java/bcd/db/ConnectionFactory.java
@@ -49,13 +49,6 @@ public abstract class ConnectionFactory {
             // carregando as propriedades user, password e useSSL do arquivo database.properties
             properties.load(getInputStream());
 
-            //Pra gerar o arquivo .jar pelo InteliJ tive que deixar os dados de conexao aqui.
-            //String host = "ampto.sj.ifsc.edu.br";
-            //String port = "33006";
-            //String dbname = "pp01renan";
-            //String user = "renan";
-            //String password = "bcd1234";
-
             // obtendo valores para host, port e dbname do arquivo properties
             String host = properties.getProperty("host");
             String port = properties.getProperty("port");
